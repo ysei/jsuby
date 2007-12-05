7ee5dd96d508fe25c116151b9a6c43487dabd105
nshuyo <nshuyo@d0d07461-0603-4401-acd4-de1884942a52>
2007-12-05 05:38:40 +0000

    lang/javascript/jsruby: added jjsruby ( JSRuby for Rhino :D )
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/jsruby@2472 d0d07461-0603-4401-acd4-de1884942a52

---
+++ trunk/tools/jjsruby.js
@@ -0,0 +1,38 @@
+// jsruby command line READ-EVAL-PRINT-LOOP.
+// (this command uses spidermonkey :)
+
+importPackage(java.io);
+var stdin = new BufferedReader(new InputStreamReader(java.lang.System["in"]));
+var stdout = new OutputStreamWriter(java.lang.System["out"]);
+
+// this is not a browser :)
+navigator = {
+    userAgent : 'DUMMY'
+}
+
+// load jsruby libraries.
+load("src/head.js");
+load("src/node.js");
+load("src/builtinobjects.js");
+load("src/parse.js");
+load("src/interpreter.js");
+load("src/builtinmethods.js");
+load("src/util.js");
+
+// setup
+var parser = new RubyEngine.Parser();
+var ruby = new RubyEngine.Interpreter();
+ruby.writeStdout = function(st) { print(st.replace(/\n$/, "")); }
+function alert(st) {
+  print(st);
+}
+
+// do loop
+stdout.write("> ");stdout.flush();
+while (line = stdin.readLine()) {
+  var nodetree = parser.parse(line)
+  //print(nodetree.toSource());
+  print(ruby.exec(nodetree));
+  stdout.write("> ");stdout.flush();
+}
+
