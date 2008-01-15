7f8e32a4ab9878955e62443e85dd26ee900947a6
nshuyo <nshuyo@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-15 03:05:56 +0000

    lang/javascript/jsruby/trunk/tools/console.js: fixed a bug
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/jsruby@4620 d0d07461-0603-4401-acd4-de1884942a52

--- trunk/tools/console.js
+++ trunk/tools/console.js
@@ -53,7 +53,7 @@ w.style.lineHeight='13px';
 w.style.backgroundColor='#999';
 w.style.border='1px solid #333';
 w.style.zIndex=9999;
-w.innerHTML = "<div style='margin:0;float:right;font-size:10px;height:10px;'><a href='javascript:void(0);' onClick='javascript:$('jsruby_window').style.display='none';'>[x]</a></div><div id='jsruby_titlebar' style='margin:0 5px;border:0;border-left:12px solid #b71234;padding:0 3px;font-size:12px;'>JSRuby Console</div><pre id='jsruby_console' style='width:320px;height:200px;margin:1px 5px;border:1px solid #333;background-color:#fff;font-size:12px;text-align:left;overflow:auto;clear:both;'></pre><input id='jsruby_input' style='width:320px;margin:1px 4px;background-color:#fff;font-size:12px;' />";
+w.innerHTML = "<div style='margin:0;float:right;font-size:10px;height:10px;'><a href='javascript:void(0);' onClick='javascript:$(\"jsruby_window\").style.display=\"none\";'>[x]</a></div><div id='jsruby_titlebar' style='margin:0 5px;border:0;border-left:12px solid #b71234;padding:0 3px;font-size:12px;'>JSRuby Console</div><pre id='jsruby_console' style='width:320px;height:200px;margin:1px 5px;border:1px solid #333;background-color:#fff;font-size:12px;text-align:left;overflow:auto;clear:both;'></pre><input id='jsruby_input' style='width:318px;margin:1px 4px;background-color:#fff;font-size:12px;' />";
 //document.body.appendChild(w);
 document.body.insertBefore(w, document.body.firstChild);
 
@@ -72,7 +72,7 @@ if(s){
   y=pos[1];
 }
 if(code && code!="") {
-  code=code.replace(/\r?\n|\r/g,"\n");
+  code=code.replace(/\n?\r/,"\n");
   jsruby_exec(code);
   w.style.left=x+'px';
   w.style.top=y+'px';
